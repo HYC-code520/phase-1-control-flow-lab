9e95a357e63fdfa451e2ca12506180509bc96828
Ariel Chen <happilyeverafter0800@gmail.com>
2024-11-22 16:47:06 -0500

    finish lab

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+   if ( distance <= 400) {
+   return 'This one is on me!';
+  }
+  else if ( distance > 400 && distance <= 2000) {
+    return 'That will be twenty bucks.';
+  }
+  else if (distance > 2000 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (distance > 2500) {
+    return 'No can do.';s
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (!city) {
+    // Handle null, undefined, or empty string
+    return 'Invalid city input.';
+  } 
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipAmount){
   // Write your code here!
+  switch (tipAmount) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
